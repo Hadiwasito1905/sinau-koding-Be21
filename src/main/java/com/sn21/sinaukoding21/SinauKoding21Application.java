b7431a51d3fde4832368bdf2adc764985ccef87e
Noverry Ambo <noverryambo@gmail.com>
2022-08-03 13:49:12 +0700

    project initiate

---
+++ src/main/java/com/sn21/sinaukoding21/SinauKoding21Application.java
@@ -0,0 +1,13 @@
+package com.sn21.sinaukoding21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinauKoding21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinauKoding21Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sn21/sinaukoding21/SinauKoding21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sn21.sinaukoding21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinauKoding21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
